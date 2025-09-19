1a15d24e8ecb5e29ed0bd8fc945d03e72dd04dcc
gusta_silva <gustavosilva18052005@gmail.com>
2025-09-18 21:04:10 -0300

    add: workflow to deploy aws

--- provider.tf
+++ provider.tf
@@ -8,16 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    rds            = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-  }
+  region = "us-east-1"
 }
